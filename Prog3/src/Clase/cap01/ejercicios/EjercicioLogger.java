e256a18b8407bf823c11051cf369d4c7adc1475a
Jon Díaz <jondi@opendeusto.es>
2019-10-17 08:47:26 +0200

    Ejemplo logger.

--- Prog3/src/Clase/cap01/ejercicios/EjercicioLogger.java
+++ Prog3/src/Clase/cap01/ejercicios/EjercicioLogger.java
@@ -3,6 +3,12 @@ package Clase.cap01.ejercicios;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
+import java.util.Date;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import javax.swing.*;
 
@@ -16,13 +22,21 @@ import javax.swing.*;
  */
 public class EjercicioLogger {
 	
+	static Logger log;
+	
 	// TODO Añadir logger y registrar lo indicado en la cabecera (y más si se quiere)
 	
 	private static JTextField tfEntrada = new JTextField( 60 );
 	private static JLabel lMensaje = new JLabel( " " );
 	public static void main(String[] args) {
+		try {
+			log = Logger.getLogger("Ejemplo_logger");
+			log.addHandler(new FileHandler("log_ejemplo.xml", true));
+		} catch (Exception e) {
+		}
 		// Ventana de ejemplo para el ejercicio
 		final JFrame f = new JFrame( "Ventana rápida para ejercicio logger" );  // Ventana a visualizar
+		log.log(Level.INFO, "Creada una ventana nueva.");
 		f.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
 		f.setSize( 800, 150 );
 		f.setLocationRelativeTo( null );
@@ -39,6 +53,8 @@ public class EjercicioLogger {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				sacaFicheros();
+				log.log(Level.INFO, "Editado el textfield.");
+
 			}
 		});
 		bBuscar.addActionListener( new ActionListener() {
@@ -52,6 +68,7 @@ public class EjercicioLogger {
 					File dir = f.getSelectedFile();
 					tfEntrada.setText( dir.getAbsolutePath() );
 					sacaFicheros();
+					log.log(Level.INFO, "Pulsado el botón buscar.");
 				}
 			}
 		});
@@ -60,9 +77,16 @@ public class EjercicioLogger {
 	
 	// Saca el número de ficheros de la carpeta indicada
 	private static  void sacaFicheros() {
-		File f = new File( tfEntrada.getText() );
-		File[] listDir = f.listFiles();
-		lMensaje.setText( "Ficheros+directorios en la carpeta: " + listDir.length );
+		try {
+			File f = new File( tfEntrada.getText() );
+			File[] listDir = f.listFiles();
+			lMensaje.setText( "Ficheros+directorios en la carpeta: " + listDir.length );
+			log.log(Level.INFO, "Número de ficheros en la carpeta " + f.getName() + ": " + listDir.length);
+		} catch (Exception e) {
+			log.log(Level.SEVERE, "Error al sacar Ficheros.", e);
+		}
+	
+
 	}
 
 }
