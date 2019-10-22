fa11cb0671bb5885e4d039347f7d1886fdc9f657
Jon Díaz <jondi@opendeusto.es>
2019-10-22 16:57:18 +0200

    Ejercicio Java Funcional

--- Prog3/src/Clase/cap01/ejercicios/JavaFuncional.java
+++ Prog3/src/Clase/cap01/ejercicios/JavaFuncional.java
@@ -17,12 +17,34 @@ public class JavaFuncional {
 	private static JLabel lSalida = new JLabel( " " );
 	private static JTextField tfEntrada = new JTextField( 20 );
 	private static JButton bProcesar;
+	private static ArrayList<String> nombreOpcion = new ArrayList<>();
+	private static ArrayList<RecibeListaInts> nombreCodigo = new ArrayList<>();
+	
+	private static void init() {
+		nombreOpcion.add("Visualizar cada 2 segundos.");//Si se pulsa la 1 llama al codigo de posicion 1, etc
+		nombreOpcion.add("Calcular media.");
+		nombreOpcion.add("Visualizar lista.");
+		nombreCodigo.add(JavaFuncional::cada2seg); //Código opción 1 //Eso indica el código en el método cada2seg en la clase JavaFuncional
+		nombreCodigo.add(JavaFuncional::calculaMedia); //Código opción 2
+		nombreCodigo.add((l) -> {lSalida.setText(l.toString());}); //Código opción 3
+		for (String s : nombreOpcion) {
+			cbOpciones.addItem(s);
+			}
+	}
+
+	private static JComboBox<String> cbOpciones = new JComboBox<>();
+	
+	static { //Al poner esto, se ejecuta siempre al iniciar la clase y sólo una vez.
+			//Puedes meter aquí lo que quieras.
+		init();
+	}
 	
 	/** Crea ventana de ejemplo con un cuadro de texto y un botón
 	 * USAR JAVA FUNCIONAL (NO ACTION LISTENER) TODO TODO
 	 * @param args	No utilizado
 	 */
 	public static void main(String[] args) {
+//		init();
 		// Creación y configuración ventana
 		JFrame f = new JFrame( "Ejemplo de lambda en Java 8" );
 		f.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
@@ -33,6 +55,7 @@ public class JavaFuncional {
 		pEntrada.add( tfEntrada );
 		pEntrada.add( bProcesar );
 		f.add( pEntrada, BorderLayout.NORTH );
+		f.add(cbOpciones, BorderLayout.CENTER);
 		f.add( lSalida, BorderLayout.SOUTH );
 		// Visualizar
 		f.pack();
@@ -59,7 +82,7 @@ public class JavaFuncional {
 //		A todo esto se le llama Sentencia Lambda.
 //		ActionListener obj = new ActionListener() {
 //			
-//			@Override
+//			@Override+63
 //			public void actionPerformed(ActionEvent arg0) {
 //				//El arg0 registra el objeto de donde viene el suceso. Al
 //				//hacer click, se ejcuta el código sobre el arg0, que es de donde viene el suceso.
@@ -117,28 +140,46 @@ public class JavaFuncional {
 		ArrayList<String> lStrings = listaDeStrings(tfEntrada.getText());
 		ArrayList<Integer> lEnteros = listaDeInts(lStrings);
 		if(lEnteros == null || lEnteros.isEmpty()) return;
+		if(cbOpciones.getSelectedIndex() != -1) {
+			nombreCodigo.get(cbOpciones.getSelectedIndex()).procesaListaEnteros(lEnteros);
+		}
+	}
+	
+	public static interface RecibeListaInts{//El interfaz indica las características generales.
+		void procesaListaEnteros( ArrayList<Integer> l);
+	}
+	
+	private static void calculaMedia(ArrayList<Integer> lEnteros) {//Cumple que recibe array list y no devuelve ná.
+		int suma = 0;
+		for (int i : lEnteros) suma += i;
+			lSalida.setText("" + 1.0 * suma/lEnteros.size());
+	}
+	
+	private static void cada2seg(ArrayList<Integer> lEnteros) {
 //		Thread t = new Thread(new Runnable() {
-//			@Override
-//			public void run() {
-//			}
-//		});
-		
-		Thread t = new Thread(
-			() -> {//No ponemos nada en el () porque no recibe nada el método run.
-			//Lo que queramos que haga el hilo.
-				bProcesar.setEnabled(false);
-				for (int i : lEnteros) {
-					lSalida.setText(i + "");
-					try {
-						Thread.sleep(2000);
-					} catch (Exception e) {}
-					}
-					lSalida.setText("");
-					bProcesar.setEnabled(true);
-			}
-		);
-		
-		t.start();
+//		@Override
+//		public void run() {
+//		}
+//	});
+	
+	Thread t = new Thread(
+		() -> {//No ponemos nada en el () porque no recibe nada el método run.
+		//Lo que queramos que haga el hilo.
+			bProcesar.setEnabled(false);
+//			for (int i : lEnteros) { Necesitamos pasar esa lista.
+			for (int i : lEnteros) {
+
+				lSalida.setText(i + "");
+				try {
+					Thread.sleep(2000);
+				} catch (Exception e) {}
+				}
+				lSalida.setText("");
+				bProcesar.setEnabled(true);
+		}
+	);
+	
+	t.start();
 	}
 	
 	/** Devuelve un arraylist de strings partiendo de un string con comas
